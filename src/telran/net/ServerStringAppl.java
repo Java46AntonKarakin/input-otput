806d625a9fe7b6193087881a53d8694837408e5e
volnykot <volnykot3@gmail.com>
2022-10-29 17:05:47 +0300

    adds Networking

--- src/telran/net/ServerStringAppl.java
+++ src/telran/net/ServerStringAppl.java
@@ -4,7 +4,6 @@ import java.net.*;
 public class ServerStringAppl {
 public static int PORT=3000;
 	public static void main(String[] args) throws Exception{
-		@SuppressWarnings("resource")
 		ServerSocket serverSocket = new ServerSocket(PORT);
 		System.out.println("Server is listening to connections on port " + PORT);
 		while(true) {
